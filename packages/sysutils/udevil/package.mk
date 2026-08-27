2e1ae054d5265217875ed9f50de53e3354cbdaeb
Dhanar Adi Dewandaru <dhanar.adi.dewandaru@gmail.com>
2026-08-27 11:53:17 +0700

    packages: update package urls

--- packages/sysutils/udevil/package.mk
+++ packages/sysutils/udevil/package.mk
@@ -4,10 +4,10 @@
 
 PKG_NAME="udevil"
 PKG_VERSION="f2b715d1d821e4b69b2fb0864a5a178dd67877f0"
-PKG_SHA256="3351d56c553c518cb2ce7b24892a4b62d630ba4f6ebee2c3994c4be9828f0629"
+PKG_SHA256="9285649d4304e7aac303d67e9ddcc6979602296bbfd80cf984317701de3a46e9"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/alpharde/udevil"
-PKG_URL="https://github.com/alpharde/udevil/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/arnie97/udevil-ng"
+PKG_URL="https://github.com/arnie97/udevil-ng/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain systemd glib"
 PKG_LONGDESC="Mounts and unmounts removable devices and networks without a password."
 
